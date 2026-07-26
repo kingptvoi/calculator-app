5b6dbffab5f001332e2f8b0ec94375a3a19c45b4
kingptvoi <73782299+kingptvoi@users.noreply.github.com>
2026-07-26 08:23:31 +0300

    changed the logic of the program: now on a button press, the script gets the integer of the label from the button and sends it to a function instead of relying on keymapping for every single label button; added basic opperations (add, sub, div, mult); working clear button; working equals button (if you press equal repeatedly it just reruns the last opperation that was done which is cool)

--- calculator.gd
+++ calculator.gd
@@ -1,94 +1,64 @@
 extends Control
 
-var display
-var display_old
+@onready var display = $display
 
-var value: String
+var display_value: String
+
+var current_opperation: String
+
 var last_value
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	last_value = 0
-	change_display(last_value)
+	display_value = "0"
+	current_opperation = ""
+	change_display(display_value)
 
+func change_display(value) -> void:
+	display.text = str(value)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		value = value.left(value.length() - 1)
-		change_display(value)
-	pass
-
-func change_display(value):
+func on_number_pressed(number: int) -> void:
+	if display_value == "0":
+		display_value = str(number)
+	else:
+		display_value += str(number)
+	change_display(display_value)
+	print(number)
 	
-	$display.text = str(value)
-	
-func change_value(value):
-	pass
+func addition(value):
+	var result: int = display_value + value
+	change_display(display_value)
 
-func _on_4_pressed() -> void:
-	last_value = str(4)
-	value = value + last_value
-	change_display(value)
-
-
-func _on_5_pressed() -> void:
-	last_value = str(5)
-	value = value + last_value
-	change_display(value)
-
-
-func _on_6_pressed() -> void:
-	last_value = str(6)
-	value = value + last_value
-	change_display(value)
-
-
-func _on_1_pressed() -> void:
-	last_value = str(1)
-	value = value + last_value
-	change_display(value)
-
-
-func _on_2_pressed() -> void:
-	last_value = str(2)
-	value = value + last_value
-	change_display(value)
-
-
-func _on_3_pressed() -> void:
-	last_value = str(3)
-	value = value + last_value
-	change_display(value)
-
-
-func _on_comma_pressed() -> void:
-	last_value = str(".")
-	value = value + last_value
-	change_display(value)
-
-
-func _on_0_pressed() -> void:
-	last_value = str(0)
-	value = value + last_value
-	change_display(value)
-
-
-
-func _on_9_pressed() -> void:
-	last_value = str(9)
-	value = value + last_value
-	change_display(value)
-
-
-func _on_8_pressed() -> void:
-	last_value = str(8)
-	value = value + last_value
-	change_display(value)
-
-
-func _on_7_pressed() -> void:
-	last_value = str(7)
-	value = value + last_value
-	change_display(value)
+func on_symbol_pressed(symbol: String) -> void:
+	print(symbol)
+	if symbol == "clear":
+		if display_value == "0":
+			pass
+		else:
+			display_value = display_value.left(display_value.length() - 1)
+			change_display(display_value)
+			
+	if symbol == "+":
+		if display_value == "0":
+			display_value = "0"
+		else:
+			current_opperation = "+"
+			last_value = str(display_value)
+			display_value = "0"
+			change_display("0")
+			
+	if symbol == "*":
+		if display_value == "0":
+			display_value = "0"
+		else:
+			current_opperation = "*"
+			last_value = str(display_value)
+			display_value = "0"
+			change_display("0")
+			
+	if symbol == "=":
+		if current_opperation == "+":
+			display_value = str(str_to_var(display_value) + str_to_var(last_value))
+			change_display(display_value)
+		if current_opperation == "*":
+			display_value = str(str_to_var(display_value) * str_to_var(last_value))
+			change_display(display_value)
